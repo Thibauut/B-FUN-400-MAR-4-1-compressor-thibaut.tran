6a628bbee605e6a4d09b06dbb13da0c14ae2cd51
MaoKoro <maokoro@pop-os.localdomain>
2023-04-21 14:35:06 +0200

    [FIX] Time of execution

--- app/Main.hs
+++ app/Main.hs
@@ -18,9 +18,6 @@ distance :: [Int] -> [Int] -> Double
 distance p1 p2 =
     sqrt (((fromIntegral (p1 !! 2)) - (fromIntegral (p2 !! 2))) ** 2 + ((fromIntegral (p1 !! 3)) - (fromIntegral (p2 !! 3))) ** 2 + ((fromIntegral (p1 !! 4)) - (fromIntegral (p2 !! 4))) ** 2)
 
--- distance :: Point -> Point -> Double
--- distance x y = sqrt $ sum $ zipWith (\a b -> (a - b) ** 2) x y
-
 myOpen :: String -> IO String
 myOpen filepath = do
     handle <- openFile filepath ReadMode
@@ -60,13 +57,6 @@ initCentroids k points = do
   indices <- mapM (\_ -> randomRIO (0, n-1)) [1..k]
   return $ map (points !!) indices
 
--- clusterError :: [Cluster] -> Double
--- clusterError clusters = sum $ concatMap clusterError' clusters
---   where
---     clusterError' cluster =
---       let centroid = calculateCentroid cluster
---       in map (distance centroid) cluster
-
 isConverged :: [Centroid] -> [Centroid] -> Double -> Bool
 isConverged oldCentroids newCentroids converged = do
     let tmp = zipWith distance oldCentroids newCentroids
@@ -76,17 +66,21 @@ isConverged oldCentroids newCentroids converged = do
     else
         False
 
+getPoint :: [Point] -> Int -> Int -> Point
+getPoint (x:xs) pos count | count == pos = x
+                          | otherwise = getPoint xs pos (count + 1)
+
 printIndex :: Index -> [Point] -> Int -> Int -> IO ()
 printIndex [] points n count = return ()
 printIndex (x:xs) points n count = do
     if x == n then do
         putStr "("
-        putStr (show (points !! count !! 0))
+        putStr (show ((getPoint points count 0) !! 0))
         putStr ","
-        putStr (show (points !! count !! 1))
+        putStr (show ((getPoint points count 0) !! 1))
         putStr ") "
         putStr "("
-        printOneCentroids (points !! count) 0
+        printOneCentroids (getPoint points count 0) 0
         putStrLn ")"
         printIndex xs points n (count + 1)
     else
@@ -126,7 +120,7 @@ checkArgumentsInt str | (readMaybe str :: Maybe Int) == Nothing = exitWith (Exit
                       | otherwise = checkArgumentsIntNext str
 
 checkArgumentsDoubleNext :: String -> IO()
-checkArgumentsDoubleNext str | (readMaybe str :: Maybe Double) < Just(0) || (readMaybe str :: Maybe Double) > Just(1)  = exitWith (ExitFailure 84)
+checkArgumentsDoubleNext str | (readMaybe str :: Maybe Double) <= Just(0) || (readMaybe str :: Maybe Double) > Just(1)  = exitWith (ExitFailure 84)
                              | otherwise = return ()
 
 checkArgumentsDouble :: String -> IO ()
@@ -164,10 +158,7 @@ main = do
     checkArgumentsDouble (args !! (getArg args "-l" 0))
     checkArgumentsInt (args !! (getArg args "-n" 0))
     file <- myOpen (args !! (getArg args "-f" 0))
-    -- centro <- (initCentroids (read (args !! (getArg args "-n" 0)) :: Int) (parsePoints file))
-    -- printCentroids centro
     res <- kMeans (read (args !! (getArg args "-n" 0)) :: Int) (read (args !! (getArg args "-l" 0)) :: Double) [] (parsePoints file)
-    -- print (floor ((calculateCentroid (getCluster res 1)) !! 2))
     return 0
 
 findClosest :: [Centroid] -> Point -> Double -> Int -> Int -> Int
@@ -185,12 +176,6 @@ closest centroids (x:xs) index = do
     let tmp = findClosest centroids x 10000.0 0 0
     tmp : closest centroids xs index
 
--- calculateNewCentroids :: [Centroid] -> [Point] -> Index -> [Centroid]
--- calculateNewCentroids [] points index = []
--- calculateNewCentroids (x:xs) points index = do
---     let tmp = calculateCentroid (getCluster points index) 0
---     tmp : calculateNewCentroids xs points (index + 1)
-
 calculatePoints :: Centroid -> [Point] -> Index -> Int -> Int -> Int -> Int
 calculatePoints centroid points [] n res pos = res
 calculatePoints centroid (y:ys) (x:xs) n res pos = do
@@ -199,31 +184,11 @@ calculatePoints centroid (y:ys) (x:xs) n res pos = do
     else
         calculatePoints centroid ys xs n res pos
 
--- calculatePoints :: Centroid -> [Point] -> Index -> Int -> Int -> Int -> Int
--- calculatePoints _ [] [] _ res _ = res
--- calculatePoints centroid (y:ys) (x:xs) n res pos
---   | x == n = calculatePoints centroid ys xs n (res + centroid !! pos) pos
---   | otherwise = calculatePoints centroid ys xs n res pos
-
 getIndex :: Index -> Int -> Int -> Int
 getIndex [] n count = count
 getIndex (x:xs) n count
     | x == n = getIndex xs n (count + 1)
     | otherwise = getIndex xs n count
-
--- calculateCentroid :: [Centroid] -> [Point] -> Index -> Int -> [Centroid]
--- calculateCentroid [] points index n = []
--- calculateCentroid (x:xs) points index n = do
---     let nbOccur = getIndex index n
---         newOccur = (fromIntegral nbOccur)
---     let r = calculatePoints x points index n 0 2
---         r2 = (fromIntegral r / newOccur)
---     let g = calculatePoints x points index n 0 3
---         g2 = (fromIntegral g / newOccur)
---     let b = calculatePoints x points index n 0 4
---         b2 = (fromIntegral b / newOccur)
---     let tmp = [(x !! 0), (x !! 1), r2, g2, b2]
---     tmp : calculateCentroid xs points index (n + 1)
 
 calculateCentroid :: [Centroid] -> [Point] -> Index -> Int -> [Centroid] -> [Centroid]
 calculateCentroid [] points index n centro  = centro
@@ -236,19 +201,5 @@ calculateCentroid (x:xs) points index n centro = do
         g2 = fromIntegral g / newOccur
     let b = calculatePoints x points index n 0 4
         b2 = fromIntegral b / newOccur
-    let tmp = [(x !! 0), (x !! 1), round r2, round g2, round b2]
+    let tmp = [(head x), (head (tail x)), round r2, round g2, round b2]
     centro
-    -- calculateCentroid xs points index (n + 1) (centro ++ [tmp])
-
--- calculateCentroid :: [Centroid] -> [Point] -> Index -> Int -> [Centroid] -> [Centroid]
--- calculateCentroid [] points index n centro = centro
--- calculateCentroid (x:xs) points index n centro = do
---         r <- calculatePoints x points index n 0 2
---         g <- calculatePoints x points index n 0 3
---         b <- calculatePoints x points index n 0 4
---     let nbOccur = getIndex index n 0
---         r2 = r `div` nbOccur
---         g2 = g `div` nbOccur
---         b2 = b `div` nbOccur
---         tmp = [x !! 0, x !! 1, r2, g2, b2]
---     in calculateCentroid xs points index (n + 1) (centro ++ [tmp])
